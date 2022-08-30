4b28edbb34d0c7b98332751cf5ebabada64b1565
Prathmun <caleb@genly.us>
2022-08-29 20:20:51 -0700

    Prototyped continuous hopper, prepping for interface update

--- hopper.py
+++ hopper.py
@@ -11,25 +11,51 @@
 
 from pathandblocks import *
 from datetime import datetime, timedelta
+from TheListBackEnd import chargechecker
 import dateutil.parser
 import random
 import time
 
 thenow=datetime.now()
+#d.get(max(d, key=d.get))
 
-def slotfiller0():	
+#2.0 to show you the block with the least charges
+
+def  slotfiller0():
     thenow=datetime.now()
-    grossblocks = []
+    grossblocks = {}
     for disc in grossdisciplines:
         for path in disc.paths:
             for block in path.blocks:
-                cooldownboolean = cooldownchecker(block)
-                if cooldownboolean == True:
-                    grossblocks.append(block)
-        try:
-            return random.choice(grossblocks)
-        except(IndexError,):
-            return systemicexit 
+            #Need to test this to see if this works for 0 charge
+                charge_count = chargechecker(block)
+                if charge_count not in grossblocks.keys():
+                    grossblocks[charge_count] = [block]
+                else:
+                    grossblocks[charge_count].append(block)
+    largest_key = max(grossblocks.keys())
+    return grossblocks[largest_key][0]
+#Also looking to see how to get the second slot to present something different.	
+
+#Might consider rolling this into the normal facade and taking out the hopper position entirely.
+#Using these functions to present needed tasks within a given discipline
+
+
+#Old version
+# def slotfiller0():	
+#     thenow=datetime.now()
+#     grossblocks = []
+#     for disc in grossdisciplines:
+#         for path in disc.paths:
+#             for block in path.blocks:
+                
+#                 cooldownboolean = cooldownchecker(block)
+#                 if cooldownboolean == True:
+#                     grossblocks.append(block)
+#         try:
+#             return random.choice(grossblocks)
+#         except(IndexError,):
+#             return systemicexit 
 				
 
 
